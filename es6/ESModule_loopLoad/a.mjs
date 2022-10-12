1762e036f258942c2259e7f16bc5b349a2b14ded
wdj <dingjie.wang@supwisdom.com>
2022-10-12 18:22:11 +0800

    module doing...

---
+++ es6/ESModule_loopLoad/a.mjs
@@ -0,0 +1,6 @@
+// a.mjs
+import { bar } from './b.mjs'
+console.log('a.mjs');
+console.log(bar());
+function foo(){ return 'foo' }
+export {foo};
---
+++ es6/ESModule_loopLoad/b.mjs
@@ -0,0 +1,6 @@
+// b.mjs
+import { foo } from './a.mjs'
+console.log('b.mjs');
+console.log(foo())
+function bar() { return 'bar' }
+export {bar};
